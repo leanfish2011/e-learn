7e4f108063b425747c48e2c57e751372796e0611
yufei <yangtze_yufei@163.com>
2024-02-04 11:05:46 +0800

    feat:static

--- README.md
+++ README.md
@@ -1,23 +1,13 @@
-# e-learn
+# 轻松学-题库
 
-> 轻松学，一键生成题库
+一个可以生成题库的网站，适用于幼儿园、小学同学
 
-## 介绍
-这是一个可以生成题库的网站，适用于幼儿园、小学同学。
+支持数学加减乘除混合运算，可设置题目，可打印题目
 
-目前支持数学加减乘除混合运算，可设置题型和难度，可打印题目
-
-## 使用
-可以在线和离线使用
-
-### 在线使用
 1. 在线访问：<a href="https://leanfish2011.github.io/e-learn" target ="_blank">轻松学-题库</a>
-2. 点击"生成"，可生成试题，可设置后生成
-3. 点击"打印"，可打印试题
 
-### 离线使用
-1. 下载zip压缩包，下载链接: https://pan.baidu.com/s/1mq_SrofNQIh-GDL9na8OSA
-提取码: 7jgn
-2. 解压zip包，用浏览器打开index.html即可访问
+2. 离线使用，下载压缩包，用浏览器打开index.html即可访问
+   * 链接: <a href="https://pan.baidu.com/s/1mq_SrofNQIh-GDL9na8OSA" target ="_blank">下载链接</a>
+   * 提取码: 7jgn
 
 使用中有任何问题或者建议，可给作者发邮件：leanfish2011@163.com
--- src/components/home/About.vue
+++ src/components/home/About.vue
@@ -2,11 +2,19 @@
   <div id="about">
     <top-header></top-header>
     <div class="aboutContent">
-      这是一个可以生成题库的网站，适用于幼儿园、小学同学。<br>
+      一个可以生成题库的网站，适用于幼儿园、小学同学<br>
       <br>
-      可设置题型和难度，可打印题目<br>
+      支持数学加减乘除混合运算，可设置题目，可打印题目<br>
       <br>
-      使用中有任何问题或者建议，可给作者发邮件：leanfish2011@163.com
+      1. 在线访问：<a href="https://leanfish2011.github.io/e-learn" target ="_blank">轻松学-题库</a><br>
+      <br>
+      2. 离线使用，下载压缩包，用浏览器打开index.html即可访问<br>
+      <br>
+      * 链接: <a href="https://pan.baidu.com/s/1mq_SrofNQIh-GDL9na8OSA" target ="_blank">下载链接</a><br>
+      <br>
+      * 提取码: 7jgn<br>
+      <br>
+      使用中有任何问题或者建议，可给作者发邮件：leanfish2011@163.com<br>
     </div>
     <bottom-footer></bottom-footer>
   </div>
